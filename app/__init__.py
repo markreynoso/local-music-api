442ea834ce51ef6418b836874c9e35962f6b54f6
Mark Reynoso <mreynoso@spu.edu>
2018-03-21 15:46:28 -0700

    migrated to mongodb, populated db, and created get route

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask import abort, jsonify, request
 
 from flask_api import FlaskAPI
 
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 from instance.config import app_config
 
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
+mongo = PyMongo()
 
 
 def create_app(config_name):
@@ -17,144 +19,158 @@ def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    mongo.init_app(app)
 
-    from .models import Band
+    # from .models import Band
 
-    @app.route('/api/bands/', methods=['GET', 'POST'])
-    def bands():
-        if request.method == 'POST':
-            name = request.data.get('name')
-            albums = request.data.get('albums')
-            location = request.data.get('location')
-            styles = request.data.get('stylesa')
-            websites = request.data.get('websites')
-            bio = request.data.get('bio')
-            if name:
-                band = Band(name=name,
-                            albums=albums,
-                            location=location,
-                            styles=styles,
-                            webites=websites,
-                            bio=bio
-                            )
-                band.save()
-                response = jsonify({
-                    'id': band.id,
-                    'name': band.name,
-                    'location': band.location,
-                    'styles': band.styles,
-                    'websites': band.websites,
-                    'bio': band.bio,
-                })
-                response.status_code = 201
-                return response
-        else:
-            bands = Band.get_all()
-            results = []
+    @app.route('/api/bands/', methods=['GET'])
+    def get_all_bands():
+        bands = mongo.db.bands
+        output = []
+        for band in bands.find():
+            output.append({'name': band['name'],
+                           'albums': band['albums'],
+                           'location': band['location'],
+                           'styles': band['styles'],
+                           'websites': band['websites'],
+                           'bio': band['bio']
+                           })
+        return jsonify({'result': output})
 
-            for band in bands:
-                obj = {
-                    'id': band.id,
-                    'name': band.name,
-                    'albums': band.albums,
-                    'location': band.location,
-                    'styles': band.styles,
-                    'websites': band.websites,
-                    'bio': band.bio
-                }
-                results.append(obj)
-            response = jsonify(results)
-            response.status_code = 200
-            return response
+    # @app.route('/api/bands/', methods=['GET', 'POST'])
+    # def bands():
+    #     if request.method == 'POST':
+    #         name = request.data.get('name')
+    #         albums = request.data.get('albums')
+    #         location = request.data.get('location')
+    #         styles = request.data.get('styles')
+    #         websites = request.data.get('websites')
+    #         bio = request.data.get('bio')
+    #         if name:
+    #             band = Band(name=name,
+    #                         albums=albums,
+    #                         location=location,
+    #                         styles=styles,
+    #                         webites=websites,
+    #                         bio=bio
+    #                         )
+    #             band.save()
+    #             response = jsonify({
+    #                 'id': band.id,
+    #                 'name': band.name,
+    #                 'location': band.location,
+    #                 'styles': band.styles,
+    #                 'websites': band.websites,
+    #                 'bio': band.bio,
+    #             })
+    #             response.status_code = 201
+    #             return response
+    #     else:
+    #         bands = Band.get_all()
+    #         results = []
 
-    @app.route('/api/bands/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-    def bands_manipulation(id, **kwargs):
-        band_search = Band.query.filter_by(id=id).first()
-        if not band_search:
-            abort(404)
+    #         for band in bands:
+    #             obj = {
+    #                 'id': band.id,
+    #                 'name': band.name,
+    #                 'albums': band.albums,
+    #                 'location': band.location,
+    #                 'styles': band.styles,
+    #                 'websites': band.websites,
+    #                 'bio': band.bio
+    #             }
+    #             results.append(obj)
+    #         response = jsonify(results)
+    #         response.status_code = 200
+    #         return response
 
-        if request.method == 'DELETE':
-            band_search.delete()
-            return {"message":
-                    "{} deleted successfully".format(band_search.id)}, 200
+    # @app.route('/api/bands/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    # def bands_manipulation(id, **kwargs):
+    #     band_search = Band.query.filter_by(id=id).first()
+    #     if not band_search:
+    #         abort(404)
 
-        elif request.method == 'PUT':
-            band_search.name = request.data.get('name')
-            band_search.albums = request.data.get('albums')
-            band_search.location = request.data.get('location')
-            band_search.styles = request.data.get('styles')
-            band_search.websites = request.data.get('websites')
-            band_search.bio = request.data.get('bio')
-            band_search.save()
-            response = jsonify({
-                'id': band_search.id,
-                'name': band_search.name,
-                'location': band_search.location,
-                'styles': band_search.styles,
-                'websites': band_search.websites,
-                'bio': band_search.bio,
-            })
-            response.status_code = 200
-            return response
-        else:
-            response = jsonify({
-                'id': band_search.id,
-                'name': band_search.name,
-                'location': band_search.location,
-                'styles': band_search.styles,
-                'websites': band_search.websites,
-                'bio': band_search.bio,
-            })
-            response.status_code = 200
-            return response
+    #     if request.method == 'DELETE':
+    #         band_search.delete()
+    #         return {"message":
+    #                 "{} deleted successfully".format(band_search.id)}, 200
 
-    @app.route('/api/bands/search/', methods=['GET', 'PUT', 'DELETE'])
-    def bands_search():
-        band_name = request.args.get('name')
-        # band_location = request.args.get('location')
-        # band_styles = request.args.get('styles')
+    #     elif request.method == 'PUT':
+    #         band_search.name = request.data.get('name')
+    #         band_search.albums = request.data.get('albums')
+    #         band_search.location = request.data.get('location')
+    #         band_search.styles = request.data.get('styles')
+    #         band_search.websites = request.data.get('websites')
+    #         band_search.bio = request.data.get('bio')
+    #         band_search.save()
+    #         response = jsonify({
+    #             'id': band_search.id,
+    #             'name': band_search.name,
+    #             'location': band_search.location,
+    #             'styles': band_search.styles,
+    #             'websites': band_search.websites,
+    #             'bio': band_search.bio,
+    #         })
+    #         response.status_code = 200
+    #         return response
+    #     else:
+    #         response = jsonify({
+    #             'id': band_search.id,
+    #             'name': band_search.name,
+    #             'location': band_search.location,
+    #             'styles': band_search.styles,
+    #             'websites': band_search.websites,
+    #             'bio': band_search.bio,
+    #         })
+    #         response.status_code = 200
+    #         return response
 
-        import pdb; pdb.set_trace()
-        band_search = Band.query.filter_by(bands.data['name'].astext == band_name).first()
-        if not band_search:
-            abort(404)
+    # @app.route('/api/bands/search/', methods=['GET', 'PUT', 'DELETE'])
+    # def bands_search():
+    #     band_name = request.args.get('name')
+    #     # band_location = request.args.get('location')
+    #     # band_styles = request.args.get('styles')
 
-        if request.method == 'DELETE':
-            band_search.delete()
-            return {"message":
-                    "{} deleted successfully".format(band_search.id)}, 200
+    #     import pdb; pdb.set_trace()
+    #     band_search = Band.query.filter_by(bands.data['name'].astext == band_name).first()
+    #     if not band_search:
+    #         abort(404)
 
-        elif request.method == 'PUT':
-            band_search.name = request.data.get('name')
-            band_search.albums = request.data.get('albums')
-            band_search.location = request.data.get('location')
-            band_search.styles = request.data.get('styles')
-            band_search.websites = request.data.get('websites')
-            band_search.bio = request.data.get('bio')
-            band_search.save()
-            response = jsonify({
-                'id': band_search.id,
-                'name': band_search.name,
-                'location': band_search.location,
-                'styles': band_search.styles,
-                'websites': band_search.websites,
-                'bio': band_search.bio,
-            })
-            response.status_code = 200
-            return response
-        else:
-            response = jsonify({
-                'id': band_search.id,
-                'name': band_search.name,
-                'location': band_search.location,
-                'styles': band_search.styles,
-                'websites': band_search.websites,
-                'bio': band_search.bio,
-            })
-            response.status_code = 200
-            return response
+    #     if request.method == 'DELETE':
+    #         band_search.delete()
+    #         return {"message":
+    #                 "{} deleted successfully".format(band_search.id)}, 200
+
+    #     elif request.method == 'PUT':
+    #         band_search.name = request.data.get('name')
+    #         band_search.albums = request.data.get('albums')
+    #         band_search.location = request.data.get('location')
+    #         band_search.styles = request.data.get('styles')
+    #         band_search.websites = request.data.get('websites')
+    #         band_search.bio = request.data.get('bio')
+    #         band_search.save()
+    #         response = jsonify({
+    #             'id': band_search.id,
+    #             'name': band_search.name,
+    #             'location': band_search.location,
+    #             'styles': band_search.styles,
+    #             'websites': band_search.websites,
+    #             'bio': band_search.bio,
+    #         })
+    #         response.status_code = 200
+    #         return response
+    #     else:
+    #         response = jsonify({
+    #             'id': band_search.id,
+    #             'name': band_search.name,
+    #             'location': band_search.location,
+    #             'styles': band_search.styles,
+    #             'websites': band_search.websites,
+    #             'bio': band_search.bio,
+    #         })
+    #         response.status_code = 200
+    #         return response
 
     # @app.route('/api/bands/load/', methods=['GET'])
     # def bands_load():
@@ -170,5 +186,24 @@ def create_app(config_name):
     #             db.session.add(entry)
     #             db.session.commit()
     #     return 'thanks'
+
+    @app.route('/api/bands/load/', methods=['GET'])
+    def load_db_bands():
+        band = mongo.db.bands
+        with open('band_data.json') as d:
+            the_data = json.load(d)
+            for entry in the_data:
+                band.insert({
+                    'name': entry,
+                    'albums': the_data[entry]['albums'],
+                    'location': the_data[entry]['location'],
+                    'styles': the_data[entry]['styles'],
+                    'websites': the_data[entry]['websites'],
+                    'bio': the_data[entry]['bio']
+                })
+        # star_id = star.insert({'name': name, 'distance': distance})
+        # new_star = star.find_one({'_id': star_id })
+        # output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+        return 'Thanks!'
 
     return app
